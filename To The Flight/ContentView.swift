94be6c4141e87bb53e07fe8e2106c24629531481
omer <46028769+sariomer96@users.noreply.github.com>
2025-02-06 22:04:25 +0300

    Initial Commit

---
+++ To The Flight/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  To The Flight
+//
+//  Created by Omer on 6.02.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ To The Flight/To_The_FlightApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_The_FlightApp.swift
+//  To The Flight
+//
+//  Created by Omer on 6.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct To_The_FlightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
